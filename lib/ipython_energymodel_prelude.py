8a5131f873d88d34644b0c29e56c229d257fa644
Daniel Friesel <derf@finalrewind.org>
2019-02-05 11:49:26 +0100

    add ipython prelude for energy modeling

---
+++ lib/ipython_energymodel_prelude.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from dfatool import EnergyModel, RawData, soft_cast_int
+
+ignored_trace_indexes = None
+
+files = '../data/20170125_125433_cc1200.tar ../data/20170125_142420_cc1200.tar ../data/20170125_144957_cc1200.tar ../data/20170125_151149_cc1200.tar ../data/20170125_151824_cc1200.tar ../data/20170125_154019_cc1200.tar'.split(' ')
+#files = '../data/20170116_124500_LM75x.tar ../data/20170116_131306_LM75x.tar'.split(' ')
+
+raw_data = RawData(files)
+preprocessed_data = raw_data.get_preprocessed_data()
+model = EnergyModel(preprocessed_data, ignore_trace_indexes = ignored_trace_indexes)
