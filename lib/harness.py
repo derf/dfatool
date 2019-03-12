bc3f8634c5bd821482bd80b904d001e43c191728
Daniel Friesel <derf@finalrewind.org>
2019-03-12 16:32:55 +0100

    harness: Use inheritance for common code

--- lib/harness.py
+++ lib/harness.py
@@ -4,48 +4,6 @@ Harnesses for various types of benchmark logs.
 tbd
 """
 
-class OnboardTimerHarness:
-    def __init__(self, gpio_pin = None):
-        self.gpio_pin = gpio_pin
-        pass
-
-    def global_code(self):
-        ret = '#include "driver/counter.h"\n'
-        ret += '#define PTALOG_TIMING\n'
-        if self.gpio_pin != None:
-            ret += '#define PTALOG_GPIO {}\n'.format(self.gpio_pin)
-        ret += '#include "object/ptalog.h"\n'
-        if self.gpio_pin != None:
-            ret += 'PTALog ptalog({});\n'.format(self.gpio_pin)
-        else:
-            ret += 'PTALog ptalog;\n'
-        return ret
-
-    def start_benchmark(self):
-        ret = 'counter.start();\n'
-        ret += 'counter.stop();\n'
-        ret += 'ptalog.passNop(counter);\n'
-        ret += 'ptalog.startBenchmark(0);\n'
-        return ret
-
-    def start_run(self):
-        return 'ptalog.reset();\n'
-
-    def pass_transition(self, transition_id, transition_code):
-        ret = 'ptalog.passTransition({:d});\n'.format(transition_id)
-        ret += 'ptalog.startTransition();\n'
-        ret += 'counter.start();\n'
-        ret += '{}\n'.format(transition_code)
-        ret += 'counter.stop();\n'
-        ret += 'ptalog.stopTransition(counter);\n'
-        return ret
-
-    def stop_run(self):
-        return 'ptalog.dump();\n'
-
-    def stop_benchmark(self):
-        return ''
-
 class TransitionHarness:
     def __init__(self, gpio_pin = None):
         self.gpio_pin = gpio_pin
@@ -80,3 +38,29 @@ class TransitionHarness:
 
     def stop_benchmark(self):
         return ''
+
+class OnboardTimerHarness(TransitionHarness):
+    def __init__(self, gpio_pin = None):
+        super().__init__(gpio_pin = gpio_pin)
+
+    def global_code(self):
+        ret = '#include "driver/counter.h"\n'
+        ret += '#define PTALOG_TIMING\n'
+        ret += super().global_code()
+        return ret
+
+    def start_benchmark(self):
+        ret = 'counter.start();\n'
+        ret += 'counter.stop();\n'
+        ret += 'ptalog.passNop(counter);\n'
+        ret += super().start_benchmark()
+        return ret
+
+    def pass_transition(self, transition_id, transition_code):
+        ret = 'ptalog.passTransition({:d});\n'.format(transition_id)
+        ret += 'ptalog.startTransition();\n'
+        ret += 'counter.start();\n'
+        ret += '{}\n'.format(transition_code)
+        ret += 'counter.stop();\n'
+        ret += 'ptalog.stopTransition(counter);\n'
+        return ret
