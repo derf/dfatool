635d9135f740a650b89026dbf198c163284bd1fa
Birte Kristina Friesel <birte.friesel@uos.de>
2025-03-24 13:27:15 +0100

    Move workload parser and evaluation to a separate class

--- bin/workload.py
+++ bin/workload.py
@@ -7,6 +7,7 @@ import sys
 import dfatool.cli
 import dfatool.utils
 from dfatool.model import AnalyticModel
+from dfatool.workload import Workload
 
 
 def main():
@@ -71,65 +72,13 @@ def main():
                 for attr in models[i].attributes(name):
                     print(f"    {name}.{attr}  {param_info(name, attr)}")
 
-    aggregate = args.aggregate_init
-    for event in args.event:
-
-        event_normalizer = lambda p: p
-        if "/" in event:
-            v1, v2 = event.split("/")
-            if dfatool.utils.is_numeric(v1):
-                event = v2.strip()
-                event_normalizer = lambda p: dfatool.utils.soft_cast_float(v1) / p
-            elif dfatool.utils.is_numeric(v2):
-                event = v1.strip()
-                event_normalizer = lambda p: p / dfatool.utils.soft_cast_float(v2)
-            else:
-                raise RuntimeError(f"Cannot parse '{event}'")
-
-        nn, param = event.split("(")
-        name, action = nn.split(".")
-        param_model = None
-        ref_model = None
-
-        for model in models:
-            if name in model.names and action in model.attributes(name):
-                ref_model = model
-                if args.use_lut:
-                    param_model = model.get_param_lut(allow_none=True)
-                else:
-                    param_model, param_info = model.get_fitted()
-                break
-
-        if param_model is None:
-            raise RuntimeError(f"Did not find a model for {name}.{action}")
-
-        param = param.removesuffix(")")
-        if param == "":
-            param = dict()
-        else:
-            param = dfatool.utils.parse_conf_str(param)
-
-        param_list = dfatool.utils.param_dict_to_list(param, ref_model.parameters)
-
-        if not args.use_lut and not param_info(name, action).is_predictable(param_list):
-            logging.warning(
-                f"Cannot predict {name}.{action}({param}), falling back to static model"
-            )
-
-        try:
-            event_output = event_normalizer(
-                param_model(
-                    name,
-                    action,
-                    param=param_list,
-                )
-            )
-        except KeyError:
-            logging.error(f"Cannot predict {name}.{action}({param}) from LUT model")
-            raise
-
-        if args.aggregate == "sum":
-            aggregate += event_output
+    workload = Workload(models)
+    aggregate = workload.eval_strs(
+        args.event,
+        aggregate=args.aggregate,
+        aggregate_init=args.aggregate_init,
+        use_lut=args.use_lut,
+    )
 
     if args.normalize_output:
         sf = dfatool.cli.parse_shift_function(
---
+++ lib/workload.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import logging
+from . import utils
+
+logger = logging.getLogger(__name__)
+
+
+class Workload:
+    def __init__(self, models):
+        self.models = models
+
+    def _event_normalizer(self, event):
+        event_normalizer = lambda p: p
+        if "/" in event:
+            v1, v2 = event.split("/")
+            if utils.is_numeric(v1):
+                event = v2.strip()
+                event_normalizer = lambda p: utils.soft_cast_float(v1) / p
+            elif utils.is_numeric(v2):
+                event = v1.strip()
+                event_normalizer = lambda p: p / utils.soft_cast_float(v2)
+            else:
+                raise RuntimeError(f"Cannot parse '{event}'")
+        return event, event_normalizer
+
+    def eval_strs(self, events, aggregate="sum", aggregate_init=0, use_lut=False):
+        for event in events:
+            event, event_normalizer = self._event_normalizer(event)
+            nn, param = event.split("(")
+            name, action = nn.split(".")
+            param_model = None
+            ref_model = None
+
+            for model in self.models:
+                if name in model.names and action in model.attributes(name):
+                    ref_model = model
+                    if use_lut:
+                        param_model = model.get_param_lut(allow_none=True)
+                    else:
+                        param_model, param_info = model.get_fitted()
+                    break
+
+            if param_model is None:
+                raise RuntimeError(f"Did not find a model for {name}.{action}")
+
+            param = param.removesuffix(")")
+            if param == "":
+                param = dict()
+            else:
+                param = utils.parse_conf_str(param)
+
+            param_list = utils.param_dict_to_list(param, ref_model.parameters)
+
+            if not use_lut and not param_info(name, action).is_predictable(param_list):
+                logging.warning(
+                    f"Cannot predict {name}.{action}({param}), falling back to static model"
+                )
+
+            try:
+                event_output = event_normalizer(
+                    param_model(
+                        name,
+                        action,
+                        param=param_list,
+                    )
+                )
+            except KeyError:
+                logging.error(f"Cannot predict {name}.{action}({param}) from LUT model")
+                raise
+
+            if aggregate == "sum":
+                aggregate_init += event_output
+            else:
+                raise RuntimeError(f"Unknown aggregate type: {aggregate}")
+
+        return aggregate_init
