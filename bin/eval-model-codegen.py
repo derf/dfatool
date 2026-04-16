d50f2c7bd53efe82721935f9824fabd1e1c3045c
Birte Kristina Friesel <birte.friesel@uos.de>
2026-04-16 14:41:22 +0200

    Codegen: compare C/C++ with Python queries (baseline)

--- bin/eval-model-codegen.py
+++ bin/eval-model-codegen.py
@@ -291,3 +291,29 @@ if __name__ == "__main__":
             f"[::] {args.model} | e_type={args.type} e_impl={impl.name} n_features={len(param_names)} n_nodes={model.get_number_of_nodes()} n_leaves={model.get_number_of_leaves()}"
             + f" depth={model.get_max_depth()} complexity={model.get_complexity_score()} {hyper_str} | rom_B={rom} ram_B={ram} {str_percentiles}"
         )
+
+    # Impl: Python (native CART / XGB)
+
+    latencies = list()
+    start = time.monotonic()
+    stop = time.monotonic()
+    nop_ns = stop - start
+    for param_tuple in itertools.product(*impl.param_values):
+        start = time.monotonic()
+        model.eval(param_tuple, cast=int if "int" in args.type else float)
+        stop = time.monotonic()
+        latencies.append((stop - start) * 1e9)
+    percentiles = np.percentile(latencies, range(0, 101))
+    str_percentiles = " ".join(
+        map(lambda kv: f"p{kv[0]:03d}_ns={kv[1]}", zip(range(0, 101), percentiles))
+    )
+    hyper_str = " ".join(
+        map(
+            lambda kv: kv[0].split("/")[1].replace(" ", "_") + f"={kv[1]}",
+            model.hyper_to_dref().items(),
+        )
+    )
+    print(
+        f"[::] {args.model} | e_type={args.type} e_impl=python n_features={len(param_names)} n_nodes={model.get_number_of_nodes()} n_leaves={model.get_number_of_leaves()}"
+        + f" depth={model.get_max_depth()} complexity={model.get_complexity_score()} {hyper_str} | rom_B=0 ram_B=0 {str_percentiles}"
+    )
